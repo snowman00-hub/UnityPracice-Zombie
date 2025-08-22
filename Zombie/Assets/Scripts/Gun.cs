d7ac6a53ae12cab38316c1754c3731d6a7942933
윤준모 <ojm01088021296@gmail.com>
2025-08-22 17:11:51 +0900

    건 추가

---
+++ Zombie/Assets/Scripts/Gun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public ParticleSystem muzzleEffect;
+    public ParticleSystem shellEffect;
+
+    private LineRenderer lineRenderer;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer.enabled = false;
+        lineRenderer.positionCount = 2;
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        yield return null;
+    }
+}
