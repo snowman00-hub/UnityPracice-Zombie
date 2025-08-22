20955d4399dca8d0a51adef3b71461caddcb6f1e
윤준모 <ojm01088021296@gmail.com>
2025-08-22 16:11:30 +0900

    라이트맵, 카메라, 애니메이션 추가

---
+++ Zombie/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public static readonly string AxisVertical = "Vertical";
+    public static readonly string AxisHorizontal = "Horizontal";
+    public static readonly string AxisFire1 = "Fire1";
+    public static readonly string AxisReload = "Reload";
+
+    public float Move { get; private set;}
+    public float Rotate { get; private set;}
+    public bool Fire { get; private set;}
+    public bool Reload { get; private set;}
+
+    private void Update()
+    {
+        Move = Input.GetAxis(AxisVertical);
+        Rotate = Input.GetAxis(AxisHorizontal);
+        Fire = Input.GetButton(AxisFire1);
+        Reload = Input.GetButton(AxisReload);
+    }
+}
---
+++ Zombie/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private static readonly int hashMove = Animator.StringToHash("Move");
+
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 180f;
+
+    private Rigidbody rb;
+    private PlayerInput input;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        input = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        // 회전
+        var rotation = Quaternion.Euler(0f, input.Rotate * rotateSpeed * Time.deltaTime, 0f);
+        rb.MoveRotation(rb.rotation * rotation);
+
+        // 이동
+        var distance = input.Move * moveSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + distance * transform.forward);
+        
+        animator.SetFloat(hashMove, input.Move);
+    }
+}
